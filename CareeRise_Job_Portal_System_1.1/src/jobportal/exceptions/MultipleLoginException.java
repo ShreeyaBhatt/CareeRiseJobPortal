b44c9fd5670dd9df1301754c273704af15b90a6f
ShreeyaBhatt <167785059+ShreeyaBhatt@users.noreply.github.com>
2026-03-03 01:40:39 +0530

    Delete CareeRise_Job_Portal_System_1.1 directory

--- CareeRise_Job_Portal_System_1.1/src/jobportal/exceptions/MultipleLoginException.java
+++
@@ -1,9 +0,0 @@
-package jobportal.exceptions;
-
-public class MultipleLoginException extends Exception {
-
-    // Constructor to create exception when a user tries multiple logins
-    public MultipleLoginException(String message) {
-        super(message); // Pass message to Exception superclass
-    }
-}
